31a358fd7e4822f23118e71853613bd12f9d9e88
ShellHase <krieger.jacke@gmail.com>
2023-02-09 17:23:00 +0100

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { View, Text, PermissionsAndroid } from 'react-native';
+
+import { BleManager, Device } from "react-native-ble-plx";
+
+const BLEManager = new BleManager();
 
 export default function App() {
+  PermissionsAndroid.requestMultiple(
+    [
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN,
+      PermissionsAndroid.PERMISSIONS.BLUETOOTH_ADVERTISE,
+      PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT
+    ]
+  ).then((answer) => {
+    console.log("LOG: scanning");
+    BLEManager.startDeviceScan(null, null, (error, scannedDevice) => {
+      if (error) {
+        console.warn(JSON.stringify(error));
+      }
+      if (scannedDevice) {
+        BLEManager.stopDeviceScan();
+        console.log("Connectoing to device: " + scannedDevice);
+        scannedDevice.connect().then((device) => {
+          console.log(device.discoverAllServicesAndCharacteristics());
+        })
+      }
+    })
+  })
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>Test</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
